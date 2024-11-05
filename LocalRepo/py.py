282c5cd7c3535cf7c79fa880cbc9541eb3477ef4
ByteCrafter0 <prajwalmishra19@gmail.com>
2024-11-05 10:48:21 +0545

    Nested loop in python

--- LocalRepo/py.py
+++ LocalRepo/py.py
@@ -1,11 +1,10 @@
-import bisect
+nums = [1, 2, 3, 4, 5]
 
-numbers = [2, 4, 6, 8, 10]
-
-# Find the index to insert 7
-index = bisect.bisect_left(numbers, 7)
-print(index)  # Output: 3
-
-# Insert 7 at the found index
-bisect.insort_left(numbers, 7)
-print(numbers)  # Output: [2, 4, 6, 7, 8, 10]
+# Nested loops example
+for i in range(len(nums)):
+    for j in range(i , len(nums)):
+        if nums[i] == nums[j]:
+            print("Duplicate found!")  # O(n^2)
+        else :
+            print("no data found")
+            break
